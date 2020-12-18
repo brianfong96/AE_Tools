ab5498e8817ed2699a586b597a446792303ef475
brianfong96 <brianwkfong@gmail.com>
2020-12-18 02:21:59 -0800

    Get relics into unity

---
+++ Unity/Relicalc/Assets/_scripts/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculator : MonoBehaviour
+{
+    [SerializeField] private TextAsset relics;
+    [SerializeField] private List<Relic> AllRelics;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AllRelics = new List<Relic>();
+        var rows = relics.text.Split('\n');
+
+        for (int i = 1; i < rows.Length; i++)
+        {
+            var rawRelic = rows[i].Split('\t');
+            var relic = new Relic(rawRelic[0], rawRelic[1], rawRelic[2], rawRelic[3], rawRelic[4]);
+            AllRelics.Add(relic);
+            Debug.Log(relic.ToString());            
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity/Relicalc/Assets/_scripts/Relic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Relic
+{
+    public string Id { get; }
+    public string Name { get; }
+    public string Quality { get; }
+    public int TotalCost { get; }
+    public string[] Components { get; }
+
+    public Relic(string id, string name, string quality, string totalCost, string components)
+    {
+        Id = id;
+        Name = name;
+        Quality = quality;
+        TotalCost = Int32.Parse(totalCost);
+        Components = components.Trim().Split(',');
+    }
+
+    public override string ToString()
+    {
+        return $"Id = {Id}\nName = {Name}\nQuality = {Quality}\nTotal Cost = {TotalCost}\nComponents = {string.Join(", ", Components)}";
+    }
+}
