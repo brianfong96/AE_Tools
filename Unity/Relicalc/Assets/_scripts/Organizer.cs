e38931ff79949526be1e41756833b300664c8986
brianfong96 <brianwkfong@gmail.com>
2020-12-18 22:35:29 -0800

    need to rethink

---
+++ Unity/Relicalc/Assets/_scripts/Organizer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Organizer : MonoBehaviour
+{
+    [SerializeField] private Vector3 startPos;
+    [SerializeField] private float shift;
+
+}
--- Unity/Relicalc/Assets/_scripts/RelicButton.cs
+++ Unity/Relicalc/Assets/_scripts/RelicButton.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 using TMPro;
 
 public class RelicButton : MonoBehaviour
@@ -12,6 +13,8 @@ public class RelicButton : MonoBehaviour
     [SerializeField] private Dropdown levels;
     [SerializeField] private TextMeshProUGUI display;
     [SerializeField] private int position;
+    [SerializeField] private GameObject added;
+    [SerializeField] private GameObject addedRelicButton;
 
     // Update is called once per frame
     public void Update()
@@ -24,5 +27,12 @@ public class RelicButton : MonoBehaviour
         
         // Debug.Log(classes.value);
         display.text = calculator.Relics.Branches[classes.captionText.text].LevelBranches[Int32.Parse(levels.captionText.text)].Positions[position].Name;
+    }
+
+    public void AddRelic()
+    {
+        var newRelic = GameObject.Instantiate(addedRelicButton);
+        newRelic.GetComponentInChildren<Text>().text = display.text;
+        newRelic.transform.SetParent(added.transform);
     }
 }
